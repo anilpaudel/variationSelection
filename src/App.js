5ec7df482b1a3b476c58b853a3faad24a3b9f148
anilpaudel <anil59paudel@gmail.com>
2021-05-08 17:02:06 +0545

    clean unused imports

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import { green } from "@material-ui/core/colors";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
-import Favorite from "@material-ui/icons/Favorite";
-import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 import variationFunction from "./variationfunction";
 import variationData from "./variationData";
 
-const GreenCheckbox = withStyles({
-  root: {
-    color: green[400],
-    "&$checked": {
-      color: green[600],
-    },
-  },
-  checked: {},
-})((props) => <Checkbox color="default" {...props} />);
+
 
 export default function CheckboxLabels() {
   const handleChangeValue = (event, item) => {
